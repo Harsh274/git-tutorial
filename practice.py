8caecd641039da5c9ee66e1fd1a2bbfe96ab343e
harshit <harshitjoshi798@gmail.com>
2022-07-14 13:17:58 +0530

    Added ecommerce contact page

--- practice.py
+++ practice.py
@@ -13,5 +13,34 @@
 #                 lower=mid-1
 
 # l=[[1,4,5],[6,7,9],[10,11,12],[23,34,45]]
-# print(sorting(l,6))
+#abstraction means 
+# from abc import *
+# class Vehicle(ABC):
+#     @abstractmethod
+#     def getnoofwheels(self):
+#         pass
+
+# class Bus(Vehicle):
+#     def getnoofwheels(self):
+#         return 8
+# b=Bus()
+# print(b.getnoofwheels())
+
+# doubt in explanation- return(max(d,key=d.get))
+'''
+
+
+
+
+
+
+'''
+
+      
+
+
+
+
+
+
 
